a91bf6a75f8ae88b67c807d96a5fcf3df7a52a6d
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-06 04:24:05 -0800

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -1,9 +1,8 @@
 import asyncio
 import difflib
 import shlex
-import sys
 from typing import Tuple
-
+import sys
 
 # if any requirements are cahnged then install that requirement
 async def lines_differnce(file1, file2):
@@ -38,16 +37,16 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
     )
 
 
-async def update_requirements(main, test):
+async def update_requirements(main , test):
     a, r = await lines_differnce(main, test)
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"💫✨🤟🏻- Installing -💫✨🤟🏻 {i}")
+            print(f" 💥©༒ʟɛɢɛռɖ✘-թπο💥 {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(update_requirements(sys.argv[1], sys.argv[2]))
+loop.run_until_complete(update_requirements(sys.argv[1] , sys.argv[2]))
 loop.close()
